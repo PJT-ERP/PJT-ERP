be7055c69a8a34a73210f6f24c17fec28d556afe
stephaniiecdr <stephanie.cdr18@gmail.com>
2026-06-12 22:24:13 +0700

    tidak pakai py

--- fix_assign.py
+++
@@ -1,9 +0,0 @@
-import os
-path = r'c:\Users\stephanie\PJT Revisi 1\src\app\components\data\mockData.ts'
-with open(path, 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace("assignedTo: 'u7'", "assignedTo: 'u2'")
-
-with open(path, 'w', encoding='utf-8') as f:
-    f.write(content)
